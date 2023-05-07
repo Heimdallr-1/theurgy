298b96a87aabb88fd3bf97c9b1f0654d9e1e036c
Kli Kli <klikli@outlook.at>
2023-05-07 11:21:27 +0200

    feat: add ingredient count to calcination

--- src/main/java/com/klikli_dev/theurgy/integration/modonomicon/page/calcination/BookCalcinationRecipePageRenderer.java
+++ src/main/java/com/klikli_dev/theurgy/integration/modonomicon/page/calcination/BookCalcinationRecipePageRenderer.java
@@ -11,6 +11,8 @@ import com.klikli_dev.modonomicon.client.render.page.BookRecipePageRenderer;
 import com.klikli_dev.theurgy.content.gui.GuiTextures;
 import com.klikli_dev.theurgy.content.recipe.CalcinationRecipe;
 import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.Ingredient;
 
 public class BookCalcinationRecipePageRenderer extends BookRecipePageRenderer<CalcinationRecipe, BookCalcinationRecipePage> {
     public BookCalcinationRecipePageRenderer(BookCalcinationRecipePage page) {
@@ -38,12 +40,20 @@ public class BookCalcinationRecipePageRenderer extends BookRecipePageRenderer<Ca
         }
 
         GuiTextures.MODONOMICON_SLOT.render(poseStack, recipeX, recipeY); //render the input slot
-        this.parentScreen.renderIngredient(poseStack, recipeX + 3, recipeY + 3, mouseX, mouseY, recipe.getIngredients().get(0));
+        this.renderIngredient(poseStack, recipeX + 3, recipeY + 3, mouseX, mouseY, recipe.getIngredients().get(0), recipe.getIngredientCount());
 
         GuiTextures.MODONOMICON_SLOT.render(poseStack, recipeX + 61, recipeY); //render the output slot
         this.parentScreen.renderItemStack(poseStack, recipeX + 61 + 3, recipeY + 3, mouseX, mouseY, recipe.getResultItem(this.parentScreen.getMinecraft().level.registryAccess()));
 
         GuiTextures.MODONOMICON_ARROW_RIGHT.render(poseStack, recipeX + 40, recipeY + 7); //render the arrow
         this.parentScreen.renderItemStack(poseStack, recipeX + 36, recipeY + 20, mouseX, mouseY, recipe.getToastSymbol());
+    }
+
+    public void renderIngredient(PoseStack poseStack, int x, int y, int mouseX, int mouseY, Ingredient ingr, int count) {
+        ItemStack[] stacks = ingr.getItems();
+        if (stacks.length > 0) {
+            var stack = stacks[(this.parentScreen.ticksInBook / 20) % stacks.length].copyWithCount(count);
+            this.parentScreen.renderItemStack(poseStack, x, y, mouseX, mouseY, stack);
+        }
     }
 }
