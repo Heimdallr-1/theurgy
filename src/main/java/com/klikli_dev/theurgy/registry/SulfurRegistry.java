c9dc08d198d18168ee28422148c5f1be263ffb6e
Kli Kli <klikli@outlook.at>
2023-05-07 18:20:19 +0200

    chore: remove comments

--- src/main/java/com/klikli_dev/theurgy/registry/SulfurRegistry.java
+++ src/main/java/com/klikli_dev/theurgy/registry/SulfurRegistry.java
@@ -99,26 +99,6 @@ public class SulfurRegistry {
     public static final RegistryObject<AlchemicalSulfurItem> SULFUR_ORE =
             register(ore("sulfur"), () -> new AlchemicalSulfurItem(new Item.Properties()));
 
-    //  this.makeRecipe("iron", 2, Tags.Items.RAW_MATERIALS_IRON, salAmmoniac, 10);
-    //        this.makeRecipe(this.raw("copper"), 2, Tags.Items.RAW_MATERIALS_COPPER, salAmmoniac, 10);
-    //        this.makeRecipe(this.raw("gold"), 2, Tags.Items.RAW_MATERIALS_GOLD, salAmmoniac, 15);
-    //        this.makeRecipe(this.raw("netherite"), 2, Tags.Items.ORES_NETHERITE_SCRAP, salAmmoniac, 100);
-    //        this.makeRecipe(this.raw("uranium"), 2, this.tag("forge:raw_materials/uranium"), salAmmoniac, 10);
-    //        this.makeRecipe(this.raw("azure_silver"), 2, this.tag("forge:raw_materials/azure_silver"), salAmmoniac, 10);
-    //        this.makeRecipe(this.raw("silver"), 2, this.tag("forge:raw_materials/silver"), salAmmoniac, 10);
-    //        this.makeRecipe(this.raw("zinc"), 2, this.tag("forge:raw_materials/zinc"), salAmmoniac, 10);
-    //        this.makeRecipe(this.raw("osmium"), 2, this.tag("forge:raw_materials/osmium"), salAmmoniac, 10);
-    //        this.makeRecipe(this.raw("nickel"), 2, this.tag("forge:raw_materials/nickel"), salAmmoniac, 10);
-    //        this.makeRecipe(this.raw("lead"), 2, this.tag("forge:raw_materials/lead"), salAmmoniac, 10);
-    //        this.makeRecipe(this.raw("allthemodium"), 2, this.tag("forge:raw_materials/allthemodium"), salAmmoniac, 100);
-    //        this.makeRecipe(this.raw("unobtainium"), 2, this.tag("forge:raw_materials/unobtainium"), salAmmoniac, 100);
-    //        this.makeRecipe(this.raw("iridium"), 2, this.tag("forge:raw_materials/iridium"), salAmmoniac, 10);
-    //        this.makeRecipe(this.raw("tin"), 2, this.tag("forge:raw_materials/tin"), salAmmoniac, 10);
-    //        this.makeRecipe(this.raw("cinnabar"), 2, this.tag("forge:raw_materials/cinnabar"), salAmmoniac, 10);
-    //        this.makeRecipe(this.raw("crimson_iron"), 2, this.tag("forge:raw_materials/crimson_iron"), salAmmoniac, 20);
-    //        this.makeRecipe(this.raw("platinum"), 2, this.tag("forge:raw_materials/platinum"), salAmmoniac, 20);
-    //        this.makeRecipe(this.raw("vibranium"), 2, this.tag("forge:raw_materials/vibranium"), salAmmoniac, 20);
-
     //Common Raw Materials Sulfurs
 
     public static final RegistryObject<AlchemicalSulfurItem> IRON_RAW =
@@ -175,6 +155,13 @@ public class SulfurRegistry {
     public static final RegistryObject<AlchemicalSulfurItem> VIBRANIUM_RAW =
             register(raw("vibranium"), () -> new AlchemicalSulfurItem(new Item.Properties()));
 
+
+    //Common Metal Ingot Sulfurs
+
+    public static final RegistryObject<AlchemicalSulfurItem> IRON_INGOT =
+            register(raw("iron"), () -> new AlchemicalSulfurItem(new Item.Properties()));
+
+
     public static <T extends Item> RegistryObject<T> register(String name, Supplier<T> sup) {
         return SULFURS.register("alchemical_sulfur_" + name, sup);
     }
@@ -189,6 +176,9 @@ public class SulfurRegistry {
 
     protected static String dust(String name) {
         return name + "_dust";
+    }
+    protected static String ingot(String name) {
+        return name + "_ingot";
     }
 
     /**
